ad508b7dc4f67f120740f1b2566a58c68f1b2cde
Pablo Matias <pmpidone@yahoo.com.ar>
2024-09-19 11:32:20 -0300

    Primera versión del Gestor Construida

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-let cantidad = 6;
+let cantidad = document.getElementById ('cantidad');
+let boton = document.getElementById ('generar');
+let contrasema = document.getElementById('contrasena');
 
-let texto = "texto";
-console.log(typeof texto);
 
-if ( cantidad < 6){
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+
+
+function generar () {
+    
+    let numerDigitado = parseInt (cantidad.value);
+    
+    if ( numerDigitado < 8){
+        alert ("La cantidad de caracters debe ser mayor que 8");
+    }
+    let password = '';
+
+    for (let i=0; i < numerDigitado; i++){
+        
+        let caracterAleatorio = cadenaCaracteres [Math.floor (Math.random() * cadenaCaracteres.length)];
+    
+        password+= caracterAleatorio;
+        
+    }
+
+    contrasema.value = password;
 }
