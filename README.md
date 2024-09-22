821610b5d0abaccc15dec0f03c5e7a1e06210053
Pablo Matias <pmpidone@yahoo.com.ar>
2024-09-21 21:31:54 -0300

    Inmersion DEV 03 - Clase 01: Proyecto final terminado

--- README.md
+++ README.md
@@ -5,7 +5,12 @@
 ### Septiembre 2024
 __________________
 
-**Primera versión construida con la guía**
+La funcionalidad es generar una contraseña en base a los caracteres mayúsculas, minusculas, números y símbolos. Y verificar el nivel de seguridad en base a que utilice por lo menos un caracter de cada grupo.
 
+**Campo cantidad:** No se genera contraseña de menos de 8 caracteres. El campo debe asignarlo el usuario comno práctica de aprendizaje para concientizar que la fortaleza de una palabra de seguridad depende en gran medida de que cuente con un número mínimo de caracteres. 
+
+**Verificar:** El sistema genera en base a una cadena pre ingresada de maenera aleatoria. Como puede generar distintas verificaciones, se pone un botón para verificar la firmeza del pasword generado.
+
+**Limpiar:** El usuario tiene la posibilidad de limpiar la interfaz para generar nuevas opciones de claves seguras.
 
 _[Pablo Matias Pidone](mailto:pablomatias.ph@gmail.com)_
--- main.js
+++ main.js
@@ -1,28 +1,45 @@
-let cantidad = document.getElementById ('cantidad');
-let boton = document.getElementById ('generar');
-let contrasema = document.getElementById('contrasena');
+let cantidad = document.getElementById("cantidad");
+let contrasena = document.getElementById("contrasena");
 
+const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./:;<=>?@[]^_`{|}~";
 
-const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generar() {
+    let numerDigitado = parseInt(cantidad.value);
 
-
-
-function generar () {
-    
-    let numerDigitado = parseInt (cantidad.value);
-    
-    if ( numerDigitado < 8){
-        alert ("La cantidad de caracters debe ser mayor que 8");
-    }
-    let password = '';
-
-    for (let i=0; i < numerDigitado; i++){
-        
-        let caracterAleatorio = cadenaCaracteres [Math.floor (Math.random() * cadenaCaracteres.length)];
-    
-        password+= caracterAleatorio;
-        
+    if (isNaN(numerDigitado) || numerDigitado < 8) {
+        alert("ERROR de seguridad: La cantidad de caracteres debe ser mayor que 8");
+        cantidad.value = "0";
+        return;
     }
 
-    contrasema.value = password;
+    let password = "";
+    for (let i = 0; i < numerDigitado; i++) {
+        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        password += caracterAleatorio;
+    }
+
+    contrasena.value = password;
+}
+
+function verificar(Contrasena) {
+    let verificarMinusculas = 0;
+    let verificarMayusculas = 0;
+    let verificarNumeros = 0;
+    let verificarCaracteres = 0;
+
+    if (Contrasena.match(/[a-z]/)) verificarMinusculas += 1;
+    if (Contrasena.match(/[A-Z]/)) verificarMayusculas += 1;
+    if (Contrasena.match(/[0-9]/)) verificarNumeros += 1;
+    if (Contrasena.match(/[!#$%&()*+,-./:;<=>?@[\]^_`{|}~]/)) verificarCaracteres += 1;
+
+    if (verificarMinusculas >= 1 && verificarMayusculas >= 1 && verificarNumeros >= 1 && verificarCaracteres >= 1) {
+        alert("SEGURIDAD ACTIVADA");
+    } else {
+        alert("Se aconseja generar una contraseña más segura");
+    }
+}
+
+function limpiar() {
+    cantidad.value = "0";
+    contrasena.value = "";
 }
